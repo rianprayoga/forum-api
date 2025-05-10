db51bb5c6cda90d2ef2660073e58eb6913b15e36
rian <junianto.rianp@gmail.com>
2025-05-10 13:22:05 +0700

    test: adjust mock response

--- src/Applications/use_case/_test/GetThreadUseCase.test.js
+++ src/Applications/use_case/_test/GetThreadUseCase.test.js
@@ -5,7 +5,7 @@ const GetThreadUseCase = require('../GetThreadUseCase');
 const { DetailReply, DetailThread, DetailComment } = require('../../../Domains/threads/entities/DetailThread');
 
 describe('GetThreadUseCase', () => {
-  it('should execute sucessfully', async () => {
+  it('should execute successfully', async () => {
     const threadId = 'threadId';
     const commentId = 'commentId';
     const secondCommentId = 'commentI2';
@@ -23,18 +23,27 @@ describe('GetThreadUseCase', () => {
       new DetailComment(
         {
           id: commentId,
-
+          content: 'content',
+          date: 'date',
+          username: 'username',
         },
         expectedReplies,
       ),
       new DetailComment({
         id: secondCommentId,
+        content: 'content',
+        date: 'date',
+        username: 'username',
       }),
     ];
 
     const expectedThread = new DetailThread(
       {
         id: threadId,
+        title: 'title',
+        body: 'body',
+        date: 'date',
+        username: 'username',
       },
       expectedComments,
     );
@@ -44,6 +53,10 @@ describe('GetThreadUseCase', () => {
       new DetailThread({
         comments: [],
         id: threadId,
+        title: 'title',
+        body: 'body',
+        date: 'date',
+        username: 'username',
       }),
     ));
 
@@ -51,20 +64,26 @@ describe('GetThreadUseCase', () => {
     commentRepo.getComments = jest.fn(() => Promise.resolve([
       new DetailComment({
         id: commentId,
+        content: 'content',
+        date: 'date',
+        username: 'username',
         replies: [],
       }),
       new DetailComment({
         id: secondCommentId,
+        content: 'content',
+        date: 'date',
+        username: 'username',
         replies: [],
       }),
     ]));
 
     const replyRepo = new ReplyRepository();
     replyRepo.getReplies = jest.fn(() => Promise.resolve([{
-      commentid: commentId, content: 'reply', date: 'date', id: 'id', username: 'username',
+      commentid: commentId, content: 'reply', date: 'date', id: 'id', username: 'username', is_deleted: false,
     },
     {
-      commentid: commentId, content: 'reply', date: 'date', id: 'id2', username: 'username',
+      commentid: commentId, content: 'reply', date: 'date', id: 'id2', username: 'username', is_deleted: false,
     },
     ]));
 
@@ -83,7 +102,7 @@ describe('GetThreadUseCase', () => {
     expect(replyRepo.getReplies).toBeCalledWith([commentId, secondCommentId]);
   });
 
-  it('should execute with no comments sucessfully', async () => {
+  it('should execute with no comments successfully', async () => {
     const threadId = 'threadId';
 
     const threadRepo = new ThreadRepository();
@@ -91,6 +110,10 @@ describe('GetThreadUseCase', () => {
       new DetailComment({
         comments: [],
         id: threadId,
+        title: 'title',
+        body: 'body',
+        date: 'date',
+        username: 'username',
       }),
     ));
 
@@ -108,6 +131,10 @@ describe('GetThreadUseCase', () => {
     expect(result).toStrictEqual(new DetailComment({
       comments: [],
       id: threadId,
+      title: 'title',
+      body: 'body',
+      date: 'date',
+      username: 'username',
     }));
 
     expect(threadRepo.getThread).toBeCalledWith(threadId);
